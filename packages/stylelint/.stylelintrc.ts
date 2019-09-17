62a11b59905efa47c8ecfaab5a557c861ee003ba
James Adarich <jadarich@neworbit.co.uk>
2019-09-17 22:01:52 +0100

    add browser support

--- packages/stylelint/.stylelintrc.ts
+++ packages/stylelint/.stylelintrc.ts
@@ -5,9 +5,14 @@ export = {
         "stylelint-config-standard-scss"
     ],
     plugins: [
+        "stylelint-no-unsupported-browser-features",
         "stylelint-prettier"
     ],
     rules: {
+        "plugin/no-unsupported-browser-features": [ true, {
+            "browsers": ["> 1%", "Last 2 versions"],
+            "ignore": ["rem"]
+        }],
         "prettier/prettier": true
     }
 
